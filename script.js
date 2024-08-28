1109b4eda8f79f0f5b7e2ecf075ac4a661310cff
Beatryzgurgel <annabeatryzgomesgurgel@gmail.com>
2024-08-28 12:29:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let vehicele1 = prompt('Digite o nome do primeiro veiculo')
+let speed1 = prompt('Digite a velocidade do primeiro veiculo')
+let vehicele2 = prompt('Digite o nome do segundo veiculo')
+let speed2 = ('Digite a velocidade do segundo veiculo')
+if ('speed1>speed2') {
+    alert('O ${vehicle1} esta a ${speed1} km/h, e esta mais rapido que o ${vehicle2} que esta a ${speed2}km/h')
+} else if (' speed1<speed2 '){
+    alert('O ${vehicle1} esta a ${speed1}km/h, e esta mais rapido que o ${vehicle2}, que esta a ${speed2}km/h')
+} else{
+    alert('O ${vehicle1} esta a ${speed2}km/h, e esta mais rapido que o ${vehicle1}, que esta a ${speed1}km/h')
+}
+
